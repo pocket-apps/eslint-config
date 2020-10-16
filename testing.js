8a547f3d8f114e4f9593ff3f55934de797bb749a
Victor Navarro <vn4varro@gmail.com>
2020-10-16 20:49:11 +0200

    fix: missing testing rules

--- testing.js
+++ testing.js
@@ -1,4 +1,10 @@
 module.exports = {
-  extends: ['plugin:jest/recommended', 'plugin:cypress/recommended'],
+  extends: [
+    'plugin:jest/recommended',
+    'plugin:cypress/recommended',
+    'plugin:testing-library/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+  ],
   rules: {},
 }
