c4dbc1d1a61c4d10a0690c2190019f9b363f47b4
Victor Navarro <vn4varro@gmail.com>
2020-12-20 20:44:11 +0100

    feat: jest style tests

--- testing.js
+++ testing.js
@@ -1,10 +1,11 @@
 module.exports = {
   extends: [
     'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:jest-dom/recommended',
     'plugin:cypress/recommended',
     'plugin:testing-library/recommended',
     'plugin:testing-library/react',
-    'plugin:jest-dom/recommended',
   ],
   rules: {
     'cypress/no-unnecessary-waiting': 'off',
